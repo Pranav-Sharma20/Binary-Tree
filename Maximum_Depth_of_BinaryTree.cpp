f8bf5b8ae2099f9781657d19c185a069f852076f
Pranav Sharma <ps25372@gmail.com>
2022-03-08 17:10:27 +0530

    added max height of BT

---
+++ Maximum_Depth_of_BinaryTree.cpp
@@ -0,0 +1,45 @@
+// HEADER FiLES and DECLARATION
+#include<bits/stdc++.h>
+using namespace std;
+// This is TreeNode Class to define the tree node
+class TreeNode{
+    public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+};
+// Driver code
+// This is a function to initialize the new node
+TreeNode * newNode(int data) {
+    TreeNode * node = new TreeNode;
+    node -> val = data;
+    node -> left = NULL;
+    node -> right = NULL;
+    return node;
+}
+
+int MaxHeight(TreeNode * root)
+{
+    if(root == NULL) return 0;
+
+    int lh = MaxHeight(root->left);
+    int rh = MaxHeight(root->right);
+    
+    return 1+max(lh, rh);
+}
+
+int main() {
+    int i, j;
+    TreeNode * root = newNode(3);
+    root -> left = newNode(9);
+    root -> right = newNode(20);
+    root -> right -> left = newNode(15);
+    root -> right -> right = newNode(7);
+
+    int maxheight;
+    maxheight = MaxHeight(root);
+
+    cout << "Maximum Height Is : "<< maxheight ;
+
+    return 0;
+}
